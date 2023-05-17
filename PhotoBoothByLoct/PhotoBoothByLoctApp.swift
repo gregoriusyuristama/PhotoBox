e3beca60e1006424de11af2e6728527b3a78d7f4
Gregorius Yuristama <gregoriusyuristama@gmail.com>
2023-05-17 16:40:44 +0700

    Initial Commit

---
+++ PhotoBoothByLoct/PhotoBoothByLoctApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoBoothByLoctApp.swift
+//  PhotoBoothByLoct
+//
+//  Created by Gregorius Yuristama Nugraha on 5/17/23.
+//
+
+import SwiftUI
+
+@main
+struct PhotoBoothByLoctApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
